1e2b585660982c71c237370559863320fb617f59
BrianDouglas <bddrew86@gmail.com>
2020-10-19 10:04:08 -0700

    fixed issue with how we calculated greatest increase/decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,27 +12,29 @@ with open(budget_path) as budget_file:
 
     for row in budget_reader:
         budget_data.append((row[0], int(row[1])))
+        if int(row[1]) < -2000000:
+            print(f"found one {row[0]}")
 
 #initialize tabulation variables    
 total_months = len(budget_data)
 total_monies = budget_data[0][1]
 changes = []
-greatest_increase = [budget_data[0][0], budget_data[0][1]]
-greatest_decrease = [budget_data[0][0], budget_data[0][1]]
+greatest_increase = [budget_data[0][0], 0]
+greatest_decrease = [budget_data[0][0], 0]
 
 #loop through remaining data
 for i in range(1,len(budget_data)):
     #sum our money
     total_monies += budget_data[i][1]
-    #check for possible change in greatest increase/decrease
-    if budget_data[i][1] > greatest_increase[1]:
-        greatest_increase[0] = budget_data[i][0]
-        greatest_increase[1] = budget_data[i][1]
-    if budget_data[i][1] < greatest_decrease[1]:
-        greatest_decrease[0] = budget_data[i][0]
-        greatest_decrease[1] = budget_data[i][1]
     #append to list of changes
     changes.append(budget_data[i][1] -  budget_data[i-1][1])
+    #check for possible change in greatest increase/decrease
+    if changes[i-1] > greatest_increase[1]:
+        greatest_increase[0] = budget_data[i][0]
+        greatest_increase[1] = changes[i-1]
+    if changes[i-1] < greatest_decrease[1]:
+        greatest_decrease[0] = budget_data[i][0]
+        greatest_decrease[1] = changes[i-1]
 
 #output    
 print("Financial Analysis")
